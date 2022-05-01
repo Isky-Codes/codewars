fdbad73bd928f0ec770b000bb629725d3889a1a5
Isky-Codes <95683156+Isky-Codes@users.noreply.github.com>
2022-05-01 22:26:23 +0800

    Added 7kyu codewars

--- main.js
+++ main.js
@@ -27,3 +27,14 @@ function check(a, x) {
     }
 }
 
+
+// 7 Kyu //
+
+//In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+function filter_list(l) {
+  return l.filter(function(v) {return typeof v == 'number'})
+}
+
+
+// 6 Kyu //
