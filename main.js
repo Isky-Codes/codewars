ff054ed6a1ff68576a05fbd4107f242f4bb8461d
Isky-Codes <95683156+Isky-Codes@users.noreply.github.com>
2022-05-04 14:25:45 +0800

    added 7kyu codewars

--- main.js
+++ main.js
@@ -58,7 +58,22 @@ var isSquare = function(n){
   }
 }
 
+//Make a program that filters a list of strings and returns a list with only your friends name in it. If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+function friend(friends){
+  return friends.filter(element => element.length === 4);
+}
+
+//Simple enough this one - you will be given an array. The values in the array will either be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one. Your job is to return a single array that has first the numbers sorted in ascending order, followed by the strings sorted in alphabetic order. The values must maintain their original type. Note that numbers written as strings are strings and must be sorted with the other strings.
+
+function dbSort(a){
+  let filteredNumbers = a.filter(element => typeof element === 'number');
+  let sortedNumbers = filteredNumbers.sort((a,b) => a - b);
+  let filteredStrings = a.filter(element => typeof element === 'string');
+  let sortedStrings = filteredStrings.sort((a,b) => a.localeCompare(b))
+  return sortedNumbers.concat(sortedStrings);
+}
+
 
 // 6 Kyu //
 
-lalal
