e0d2d49b9e7242c66b9d480c10e3cb3fe9a97a15
Isky-Codes <95683156+Isky-Codes@users.noreply.github.com>
2022-05-06 15:02:14 +0800

    added 7kyu array codewars

--- main.js
+++ main.js
@@ -95,6 +95,20 @@ function sortByLength (array) {
   return sortedArr;
 }
 
+//Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+function flattenAndSort(array) {
+  var newArray = [];
+  for(var i =0;i<array.length;i++)
+  {
+    for(var j=0;j<array[i].length;j++)
+    {
+      newArray.push(array[i][j]);
+    }
+  }
+    
+  return newArray.sort((a,b) => a-b);
+}
+
 
 // 6 Kyu //
 
