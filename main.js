4533a440d79e4c9b2f7a6d69e9b1c70057e8b4da
Isky-Codes <95683156+Isky-Codes@users.noreply.github.com>
2022-05-06 20:52:44 +0800

    added more string problems codewars

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 // 8 Kyu //
 
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+var stringToNumber = function(str){
+  return Number(str);
+}
+
 //You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left. Considering these factors, write a function that tells you if it is possible to get to the pump or not. Function should return true (1 in Prolog, NASM and COBOL) if it is possible and false (0 in Prolog, NASM and COBOL) if not. The input values are always positive.
 
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
@@ -55,6 +61,15 @@ function arrayPlusArray(arr1, arr2) {
 
 
 // 7 Kyu //
+
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+function solution(str, ending){
+  if(str.endsWith(ending)) {
+    return true;
+  }else {
+    return false;
+  }
+}
 
 //In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
 
