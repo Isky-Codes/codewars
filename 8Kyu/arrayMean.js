fc4a979ce7dd866f1b83e53d821753f6ba6ac589
Isky-Codes <shahriman.iskandar@gmail.com>
2022-05-31 11:31:26 +0800

    add minor changes to 8kyu files

--- 8Kyu/arrayMean.js
+++ 8Kyu/arrayMean.js
@@ -1,3 +1,4 @@
-var findAverage = function (nums) {
-    return (nums.reduce((acc, curr) => acc + curr, 0)) / nums.length;
+var findAverage = function (num) {
+    return (num.reduce((acc, curr) => acc + curr, 0)) / num.length;
 };
+
