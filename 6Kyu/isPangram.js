7480da840781031b6684d1570499bad2baa61ab9
Isky-Codes <shahriman.iskandar@gmail.com>
2022-06-10 21:21:10 +0800

    Added 8kyu and 6kyu challenges

---
+++ 6Kyu/isPangram.js
@@ -0,0 +1,9 @@
+function isPangram(string){
+  let lowerStr = string.toLowerCase();
+  for(let char of 'abcdefghijklmnopqrstuvwxyz') {
+    if(!lowerStr.includes(char)) {
+      return false
+    }
+  }
+  return true
+}
